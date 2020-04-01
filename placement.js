c10f05fbed1885ad09c23dbd2ce02b3b9cd065c9
RealityTheory <49117580+RealityTheory@users.noreply.github.com>
2020-04-01 10:51:47 +0100

    Turning on debug dialogue

--- placement.js
+++ placement.js
@@ -1,20 +1,20 @@
 window.onload = () => {
   placeCursor(); 
 };
-  
-  function placeCursor() 
-  { 
-    const raycaster = document.querySelector('[ar-raycaster]');
-    const cursor = document.querySelector('#cursor');
-    raycaster.addEventListener('raycaster-intersection', (event) => {
-      cursor.setAttribute('position', event.detail.intersections[0].point);
-    });
-  }
 
-  window.unload = () => {
-    clearInterval(t);
-  };
+function placeCursor() 
+{ 
+  const raycaster = document.querySelector('[ar-raycaster]');
+  const cursor = document.querySelector('#cursor');
+  raycaster.addEventListener('raycaster-intersection', (event) => {
+    cursor.setAttribute('position', event.detail.intersections[0].point);
+  });
+}
 
-  const scene = document.querySelector('a-scene');
-  scene.addEventListener('loaded', onSceneLoaded);
-  myVar = setInterval(placeCursor, 3000);
+window.unload = () => {
+  clearInterval(t);
+};
+
+const scene = document.querySelector('a-scene');
+scene.addEventListener('loaded', onSceneLoaded);
+myVar = setInterval(placeCursor, 1000);
